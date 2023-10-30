7668ea88db61702558e4555e8f6d24f38179c1c5
kaymacharia <kaymachariaa@gmail.com>
2023-10-30 13:03:24 -0700

    1

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
-str1 = "Welcome to "
-str2 = "Holberton School!"
-
-# Print the string without using any loops or conditional statements.
-print(str1,str2)
+str1 = "Holberton"
+str2 = "School"
+str = str1 + str2
+print(f"Welcome to {str}!")

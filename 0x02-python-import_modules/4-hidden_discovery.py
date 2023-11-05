552cec7425067c2509e959878d01a0816ebd796e
kaymacharia <kaymachariaa@gmail.com>
2023-11-05 05:22:42 -0800

    1

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
-  for word in dir(hidden_4):
-      if not(word[0] == '_' and word[1] == '_'):
-            print("{}".format(word))
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)

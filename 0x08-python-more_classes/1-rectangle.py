eba844e0761bb3659bae0a77312177d415a46704
kaymacharia <kaymachariaa@gmail.com>
2023-11-27 10:24:40 -0800

    1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,16 +3,16 @@
 
 
 class Rectangle:
-    """
+"""
 A rectangle is defined by the following characteristics:
 It has private instance attributes: "width" (an integer) and "height" (an integer).
 It can be instantiated with optional values for width and height.
-    """
+"""
 
     def __init__(self, width=0, height=0):
         """ Constructor method """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):

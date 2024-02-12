4c1b8427cc3dcbcdbe99df548d357b6db2acdb35
kaymacharia <kaymachariaa@gmail.com>
2024-02-12 10:37:17 -0800

    1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const cLine = "C is fun";
+console.log(cLine);
+const pythonLine = "Python is cool";
+console.log(pythonLine);
+const jsLine = "JavaScript is amazing";
+console.log(jsLine);

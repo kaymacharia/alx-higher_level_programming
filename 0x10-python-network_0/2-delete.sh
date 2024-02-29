b924d74a0ed77d2cbc3a615ab5f343c93edd4b7e
kaymacharia <kaymachariaa@gmail.com>
2024-02-29 04:02:13 -0800

    1

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Check if an argument is provided
-if [ -z "$1" ]; then
-  echo "Error: Please provide a URL as an argument."
-  exit 1
-fi
-
-# Define the URL from the argument
-url="$1"
-
-# Use curl to send a DELETE request and display the response body
-curl -X DELETE -s "$url"
+# sends a DELETE request to the URL passed 
+curl -X DELETE -s "$1"

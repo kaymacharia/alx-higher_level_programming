68f07fc13eaf3ce0ed9d81ac60135d7a99b3642a
kaymacharia <kaymachariaa@gmail.com>
2024-02-29 03:54:24 -0800

    1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Use curl to get the response and save the body size
-response_size=$(curl -s -o /dev/null -w "%{size_download}" "$url")
+# Use curl to get the response and save 
+curl -s -o /dev/null -w '%{size_download}n' $1

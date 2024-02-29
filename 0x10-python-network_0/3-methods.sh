6b00f8035f37598b2bfcbaece996d53f108ce8bd
kaymacharia <kaymachariaa@gmail.com>
2024-02-29 04:09:27 -0800

    1

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#takes in a URL and displays all HTTP methods the server will accept
-curl -s -o /dev/null -I -w "%{header[Allow]}" "$1"
+#takes in a URL and displays all HTTP methods
+curl -sI $1 | header Allow | cut -d ' ' -f2-
